7b44b382bfe033f69ba45b2f3a41e9d3e429140d
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-03-23 00:12:01 -0400

    add frontend

---
+++ fend/src/components/navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import logo from "../images/Smart Bed Systems.png"; // relative path to image
+
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+
+// Here, we display our Navbar
+export default function Navbar() {
+  const title = {
+    float: "left",
+    borderRadius: "25px",
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial",
+  };
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink className="navbar-brand" to="/">
+          <img style={{ width: 25 + "%" }} src={logo}></img>
+        </NavLink>
+        <span style={title} className="title">
+          Smart Bed Systems User Application
+        </span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/create">
+                Create Record
+              </NavLink>
+            </li>
+          </ul>
+        </div> */}
+      </nav>
+    </div>
+  );
+}
