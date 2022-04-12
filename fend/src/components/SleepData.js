b4a5ccef0220d2f8b5b331a511e4546c96bf068f
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-04-12 03:02:31 -0400

    .

--- fend/src/components/SleepData.js
+++ fend/src/components/SleepData.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { Line, Chart } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 // const axios = require("axios");
 
 export default function SleepData() {
@@ -53,6 +55,74 @@ export default function SleepData() {
           ))}
         </tbody>
       </table>
+      <div>
+        <Line
+          data={{
+            labels: [
+              "22:00",
+              "23:00",
+              "00:00",
+              "01:00",
+              "02:00",
+              "03:00",
+              "04:00",
+              "05:00",
+              "06:00",
+            ],
+            datasets: [
+              {
+                label: "REM Detection",
+                data: [1, 2, 3, 4, 2, 4, 3, 2, 1],
+              },
+            ],
+          }}
+          height={400}
+          width={600}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              yAxis: {
+                suggestedMin: 0,
+                suggestedMax: 5,
+              },
+            },
+          }}
+        />
+      </div>
+      <div>
+        <Line
+          data={{
+            labels: [
+              "22:00",
+              "23:00",
+              "00:00",
+              "01:00",
+              "02:00",
+              "03:00",
+              "04:00",
+              "05:00",
+              "06:00",
+            ],
+            datasets: [
+              {
+                label: "Heart Rate",
+                data: [75, 73, 60, 62, 60, 68, 72, 75, 79],
+              },
+            ],
+          }}
+          height={400}
+          width={600}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              yAxis: {
+                suggestedMin: 50,
+                suggestedMax: 90,
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   );
 }
